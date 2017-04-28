d786f8df6b4ec3bdc916b63520d7dd5cfd9f5621
paramono <alex@paramono.com>
2017-04-29 01:58:12 +0300

    Add new features

--- cmsplugin_slick/cms_plugins.py
+++ cmsplugin_slick/cms_plugins.py
@@ -1,119 +1,302 @@
 import json
 from django.contrib import admin
-from cms.plugin_base import CMSPluginBase
-from cms.plugin_pool import plugin_pool
+from django.template.loader import select_template
+from django.templatetags.static import static
 from django.utils.translation import ugettext_lazy as _
 
-from filer.models.imagemodels import Image
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
 
-from .models import Carousel, CarouselBreakpoint, CarouselElementWrapper, CaroselImageFolder
+from filer.models.imagemodels import Image
+from cmsplugin_filer_image.conf import settings
+
+from .models import (
+    Carousel, CarouselBreakpoint, CarouselElementWrapper, CaroselImageFolder
+)
+
 
 class CarouselBreakpointInline(admin.StackedInline):
     model = CarouselBreakpoint
     extra = 0
 
+
 class CarouselPlugin(CMSPluginBase):
-    """
-    Main carousel plugin that parent for others plugin.
-    Supossed one plugin - one slide, or support plugins that get list of ellements, see my filler folder galery
-    """
+    '''
+    Parent slider plugin.
+
+    Any other plugin can be added as a child (as a slide), or you can use
+    special plugins that generate a list of elements (see
+    CarouselImageFolderPlugin below for one such example
+    '''
+
     model = Carousel
     render_template = "cmsplugin_slick/carousel.djhtml"
     module = 'Slick Carousel'
-    name = 'Carousel Plugin'
+    name = _('Slick Carousel')
     allow_children = True
-    inlines = [CarouselBreakpointInline,]
-    
+    inlines = [CarouselBreakpointInline, ]
+
     fieldsets = (
-        (None, {'fields': (
+        (None, {
+            'fields': (
                 'title',
                 'default_style',
                 'infinite',
                 'speed',
+                'height',
                 ('dots', 'arrows'),
                 ('slides_to_show', 'slides_to_scroll'),
-        )}),
-        
-        ('Autoplay', {
+            )
+        }),
+        (_('Autoplay'), {
             'classes': ('collapse',),
             'fields': (
                 ('autoplay', 'autoplay_speed'),
                 ('pause_on_hover', 'pause_on_dots_hover'),
-        )}),
-                 
-        ('Others Settings', {
+            )
+        }),
+        (_('Others Settings'), {
             'classes': ('collapse',),
-            'fields':(
+            'fields': (
                 'fade',
                 ('center_mode', 'center_padding'),
                 'variable_width',
                 ('vertical', 'rigth_to_left'),
-        )}),
-        
-        ('Advanced', {
+            )
+        }),
+        (_('Advanced'), {
             'classes': ('collapse',),
             'fields': (
                 'classes',
-            ),
+            )
+        }),
+        (_('Breakpoint Settings'), {
+            'fields': (
+                'auto_breakpoints', 'mobile_first',
+            )
         }),
     )
-    
+
+    def make_auto_breakpoints(self, instance):
+        # breakpoints (bootstrap):
+        # xs:  480px [  0; 768-1]
+        # sm:  768px [768; 992-1]
+        # md:  992px [992; 1200-1]
+        # lg: 1200px [1200; inf+]
+
+        def slide_decr(x):
+            return max(x-1, 1)
+
+        def slide_incr(x):
+            return max(x+1, 1)
+
+        def media(breakpoint, slidesToShow=1, slidesToScroll=1, **kwargs):
+            settings = {
+                'slidesToShow': slidesToShow,
+                'slidesToScroll': slidesToScroll,
+            }
+            settings.update(**kwargs)
+
+            media_dict = {
+                'breakpoint': breakpoint,
+                'settings': settings,
+            }
+            return media_dict
+
+        slides = instance.slides_to_show
+        if instance.mobile_first:
+            # default settings are xs
+            # mobile-first, thus means:
+            # 'from this breakpoint and above, do this'
+            slides = slide_incr(slides)
+            sm = media(768, slidesToShow=slides, slidesToScroll=1)
+
+            slides = slide_incr(slides)
+            md = media(992, slidesToShow=slides, slidesToScroll=1)
+
+            responsive = [sm, md]
+
+        else:
+            # default settings are lg+
+            # not mobile-first, thus means:
+            # 'from this breakpoint and below, do this'
+            # md = media(1200-1, slidesToShow=slides, slidesToScroll=1)
+            slides = slide_decr(slides)
+            md = media(1200-1, slidesToShow=slides, slidesToScroll=1)
+
+            slides = slide_decr(slides)
+            sm = media(992-1, slidesToShow=slides, slidesToScroll=1)
+
+            # forcing 1 slide in xs
+            xs = media(768-1, slidesToShow=1, slidesToScroll=1)
+
+            responsive = [sm, xs, md]
+
+        # output = {
+        #     'responsive': responsive
+        # }
+        return responsive
+
     def render(self, context, instance, placeholder):
-        context = super(CarouselPlugin, self).render(context, instance, placeholder)
-        slick_dict = {'infinite': instance.infinite,
-                            'speed': instance.speed,
-                            'dots': instance.dots, 'arrows': instance.arrows,
-                            'slidesToShow': instance.slides_to_show, 'slidesToScroll': instance.slides_to_scroll,
-                            'autoplay': instance.autoplay, 'autoplaySpeed': instance.autoplay_speed,
-                            'pauseOnHover': instance.pause_on_hover, 'pauseOnDotsHover': instance.pause_on_dots_hover,
-                            'fade': instance.fade,
-                            'centerMode': instance.center_mode, 'centerPadding': instance.center_padding,
-                            'variableWidth': instance.variable_width,'vertical': instance.vertical,
-                            'rtl': instance.rigth_to_left}
-        
-        if instance.breakpoints.all():
-            responsive = []
-            for breakpoint in instance.breakpoints.all():
-                responsive.append({'breakpoint': breakpoint.breakpoint,
-                                   'settings': {'slidesToShow': breakpoint.slides_to_show,
-                                                'slidesToScroll': breakpoint.slides_to_scroll}
-                                   })
-                
+        context = super(CarouselPlugin, self).render(
+            context, instance, placeholder)
+
+        slick_dict = {
+            'infinite': instance.infinite,
+            'speed': instance.speed,
+            'dots': instance.dots,
+            'arrows': instance.arrows,
+            'slidesToShow': instance.slides_to_show,
+            'slidesToScroll': instance.slides_to_scroll,
+            'autoplay': instance.autoplay,
+            'autoplaySpeed': instance.autoplay_speed,
+            'pauseOnHover': instance.pause_on_hover,
+            'pauseOnDotsHover': instance.pause_on_dots_hover,
+            'fade': instance.fade,
+            'centerMode': instance.center_mode,
+            'centerPadding': instance.center_padding,
+            'variableWidth': instance.variable_width,
+            'vertical': instance.vertical,
+            'rtl': instance.rigth_to_left,
+            'slide': ':not(template)',
+        }
+
+        if instance.auto_breakpoints:
+            responsive = self.make_auto_breakpoints(instance)
             slick_dict.update({'responsive': responsive})
-            
+        else:
+            if instance.breakpoints.all():
+                responsive = []
+                for breakpoint in instance.breakpoints.all():
+                    responsive.append({
+                        'breakpoint': breakpoint.breakpoint,
+                        'settings': {
+                            'slidesToShow': breakpoint.slides_to_show,
+                            'slidesToScroll': breakpoint.slides_to_scroll
+                        }
+                    })
+
+            slick_dict.update({'responsive': responsive})
+
         slick_settings = json.dumps(slick_dict)
-            
         context.update({'slick_settings': slick_settings})
-                
         return context
-    
+
+
 class CarouselElementWrapperPlugin(CMSPluginBase):
-    """
+    '''
     Include all child plugin as one slide
-    """
+    '''
     model = CarouselElementWrapper
     render_template = "cmsplugin_slick/element_wrapper.djhtml"
     module = 'Slick Carousel'
-    name = 'Plugins Wrapper'
-    #require_parent = True
+    name = _('Slide Wrapper')
+    # require_parent = True
     parent_classes = ['CarouselPlugin']
     allow_children = True
-    
+
+
 class CarouselImageFolderPlugin(CMSPluginBase):
     model = CaroselImageFolder
     render_template = "cmsplugin_slick/folder_carousel.djhtml"
     module = 'Slick Carousel'
-    name = 'Image Folder Carousel'
-    #require_parent = True
+    name = _('Image Folder Carousel')
+    # require_parent = True
     parent_classes = ['CarouselPlugin']
-        
+
+    fieldsets = (
+        (None, {
+            'fields': [
+                'title',
+                'folder',
+            ]
+        }),
+        (_('Image resizing options'), {
+            'fields': (
+                'use_original_image',
+                ('width', 'height',),
+                ('crop', 'upscale',),
+                'thumbnail_option',
+                'use_autoscale',
+            )
+        }),
+    )
+    if settings.CMSPLUGIN_FILER_IMAGE_STYLE_CHOICES:
+        fieldsets[0][1]['fields'].append('style')
+
+    def _get_thumbnail_options(self, context, instance):
+        """
+        Return the size and options of the thumbnail that should be inserted
+        """
+        width, height = None, None
+        crop, upscale = False, False
+        subject_location = False
+        placeholder_width = context.get('width', None)
+        placeholder_height = context.get('height', None)
+        if instance.thumbnail_option:
+            # thumbnail option overrides everything else
+            if instance.thumbnail_option.width:
+                width = instance.thumbnail_option.width
+            if instance.thumbnail_option.height:
+                height = instance.thumbnail_option.height
+            crop = instance.thumbnail_option.crop
+            upscale = instance.thumbnail_option.upscale
+        else:
+            if instance.use_autoscale and placeholder_width:
+                # use the placeholder width as a hint for sizing
+                width = int(placeholder_width)
+            elif instance.width:
+                width = instance.width
+            if instance.use_autoscale and placeholder_height:
+                height = int(placeholder_height)
+            elif instance.height:
+                height = instance.height
+            crop = instance.crop
+            upscale = instance.upscale
+
+        if not width:
+            width = 0
+        if not height:
+            height = 0
+
+        return {
+            'size': (width, height),
+            'crop': crop,
+            'upscale': upscale,
+            'subject_location': subject_location,
+        }
+
+    def get_thumbnail(self, context, instance):
+        if instance.image:
+            return instance.image.file.get_thumbnail(self._get_thumbnail_options(context, instance))
+
+    # def get_render_template(self, context, instance, placeholder):
+    #     template = select_template((
+    #         'cmsplugin_filer_image/plugins/image.html',  # backwards compatibility. deprecated!
+    #         self.TEMPLATE_NAME % instance.style,
+    #         self.TEMPLATE_NAME % 'default',
+    #     ))
+    #     return template
+
+    # def icon_src(self, instance):
+    #     if instance.image:
+    #         if getattr(settings, 'FILER_IMAGE_USE_ICON', False) and '32' in instance.image.icons:
+    #             return instance.image.icons['32']
+    #         else:
+    #             # Fake the context with a reasonable width value because it is not
+    #             # available at this stage
+    #             thumbnail = self.get_thumbnail({'width': 200}, instance)
+    #             return thumbnail.url
+    #     else:
+    #         return static("filer/icons/missingfile_%sx%s.png" % (32, 32,))
+
     def get_folder_images(self, folder, user):
         qs_files = folder.files.instance_of(Image)
         if user.is_staff:
             return qs_files
         else:
             return qs_files.filter(is_public=True)
-        
+
     def render(self, context, instance, placeholder):
         context = super(CarouselImageFolderPlugin, self).render(context, instance, placeholder)
         user = context['request'].user
@@ -123,11 +306,18 @@ class CarouselImageFolderPlugin(CMSPluginBase):
         else:
             folder_images = Image.objects.none()
 
-        context.update({'folder_images': sorted(folder_images)})
-        
+        options = self._get_thumbnail_options(context, instance)
+        context.update({
+            'folder_images': sorted(folder_images),
+            'instance': instance,
+            'opts': options,
+            'size': options.get('size', None),
+            'placeholder': placeholder
+            # 'link': instance.link,
+        })
         return context
-    
-    
+
+
 plugin_pool.register_plugin(CarouselPlugin)
 plugin_pool.register_plugin(CarouselElementWrapperPlugin)
 plugin_pool.register_plugin(CarouselImageFolderPlugin)
--- cmsplugin_slick/models.py
+++ cmsplugin_slick/models.py
@@ -1,79 +1,153 @@
-from cms.models.pluginmodel import CMSPlugin
+from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from cms.models.pluginmodel import CMSPlugin
 from filer.fields.folder import FilerFolderField
-from cmsplugin_filer_utils import FilerPluginManager
 
-from django.db import models
+from cmsplugin_filer_image.conf import settings
+
 
 # Create your models here.
 class Carousel(CMSPlugin):
-    """
+    '''
     Store main settings for carousel
-    """
-    #None
+    '''
+
+    class Meta:
+        verbose_name = _('Carousel')
+        verbose_name_plural = _('Carousels')
+
     title = models.CharField(verbose_name=_('Title'), max_length=60, blank=True)
-    
     default_style = models.BooleanField(verbose_name=_('Use default style'), default=True)
-    
     infinite = models.BooleanField(verbose_name=_('Infinite'), default=True)
-    
     speed = models.IntegerField(verbose_name=_('Speed'), default=300)
-    
+    height = models.PositiveIntegerField(_("height"), null=True, blank=True)
+
     dots = models.BooleanField(verbose_name=_('Dots'), default=True)
     arrows = models.BooleanField(verbose_name=_('Arrows'), default=True)
-    
+
     slides_to_show = models.IntegerField(verbose_name=_('Slides to show'), default=1)
     slides_to_scroll = models.IntegerField(verbose_name=_('Slides to scroll'), default=1)
-    
-    #Autoplay
+
+    # Autoplay
     autoplay = models.BooleanField(verbose_name=_('Autoplay'), default=False)
     autoplay_speed = models.IntegerField(verbose_name=_('Autoplay speed'), null=True, blank=True)
-    
+
     pause_on_hover = models.BooleanField(verbose_name=_('Pause on hover'), default=True)
     pause_on_dots_hover = models.BooleanField(verbose_name=_('Pause on dots hover'), default=False)
-    
-        
-    #Other
+
+    # Other
     fade = models.BooleanField(verbose_name=_('Fade animation'), default=False)
-    
+
     center_mode = models.BooleanField(verbose_name=_('Center mode'), default=False)
     center_padding = models.CharField(verbose_name=_('Center padding'), max_length=10, blank=True)
-    
+
     variable_width = models.BooleanField(verbose_name=_('Variable Width'), default=False)
-    
-    vertical = models.BooleanField(verbose_name=_('Vertical'), default=False)  
+
+    vertical = models.BooleanField(verbose_name=_('Vertical'), default=False)
     rigth_to_left = models.BooleanField(verbose_name=_('Right to left'), default=False)
-    
-    
-    #Advanced
+
+
+    # Advanced
     classes = models.TextField(verbose_name=_('Css classes'), blank=True)
-    
-    
+    auto_breakpoints = models.BooleanField(verbose_name=_('Auto Breakpoints'), default=True)
+    mobile_first = models.BooleanField(
+        default=False,
+        verbose_name=_('Mobile First'),
+        help_text=_('Responsive settings use mobile first calculation')
+    )
+
+
     def copy_relations(self, oldinstance):
         self.breakpoints = oldinstance.breakpoints.all()
-    
+
+
 class CarouselBreakpoint(models.Model):
     """
-    Carousel break point allow create sesponsive carousels
+    Carousel breakpoints allow to create responsive carousels
     """
     carousel = models.ForeignKey('Carousel', related_name='breakpoints', verbose_name=_('Carousel'))
-    
-    breakpoint = models.IntegerField(verbose_name=_('Breakpoint resolution'))  
-    
+    breakpoint = models.IntegerField(verbose_name=_('Breakpoint resolution'))
     slides_to_show = models.IntegerField(verbose_name=_('Slides to show'), default=1)
     slides_to_scroll = models.IntegerField(verbose_name=_('Slides to scroll'), default=1)
-    
-    
-class CarouselElementWrapper(CMSPlugin):    
+
+    class Meta:
+        verbose_name = _('Carousel Breakpoint')
+        verbose_name_plural = _('Carousel Breakpoints')
+
+
+class CarouselElementWrapper(CMSPlugin):
     classes = models.TextField(verbose_name=_('Css classes'), blank=True)
-    
+
+    class Meta:
+        verbose_name = _('Carousel Slide')
+        verbose_name_plural = _('Carousel Slides')
+
+
 class CaroselImageFolder(CMSPlugin):
+    LEFT = "left"
+    RIGHT = "right"
+    CENTER = "center"
+    FLOAT_CHOICES = ((LEFT, _("left")),
+                     (RIGHT, _("right")),
+                     (CENTER, _("center")),
+                     )
+    STYLE_CHOICES = settings.CMSPLUGIN_FILER_IMAGE_STYLE_CHOICES
+    DEFAULT_STYLE = settings.CMSPLUGIN_FILER_IMAGE_DEFAULT_STYLE
+    EXCLUDED_KEYS = ['class', 'href', 'target', ]
+
     title = models.CharField(_("Title"), max_length=255, null=True, blank=True)
     folder = FilerFolderField(null=True, on_delete=models.SET_NULL)
-    
-    #objects = FilerPluginManager(select_related=('folder',))
-    
+
+    style = models.CharField(
+        _('Style'), choices=STYLE_CHOICES, default=DEFAULT_STYLE, max_length=50, blank=True)
+    # caption_text = models.CharField(_("caption text"), null=True, blank=True, max_length=255)
+    alt_text = models.CharField(_("alt text"), null=True, blank=True, max_length=255)
+    use_original_image = models.BooleanField(
+        _("use the original image"), default=False,
+        help_text=_('do not resize the image. use the original image instead.'))
+    thumbnail_option = models.ForeignKey(
+        'filer.ThumbnailOption', null=True, blank=True, verbose_name=_("thumbnail option"),
+        help_text=_('overrides width, height, crop and upscale with values from the selected thumbnail option'))
+    use_autoscale = models.BooleanField(_("use automatic scaling"), default=False,
+                                        help_text=_('tries to auto scale the image based on the placeholder context'))
+    width = models.PositiveIntegerField(_("width"), null=True, blank=True)
+    height = models.PositiveIntegerField(_("height"), null=True, blank=True)
+    crop = models.BooleanField(_("crop"), default=True)
+    upscale = models.BooleanField(_("upscale"), default=True)
+    # alignment = models.CharField(_("image alignment"), max_length=10, blank=True, null=True, choices=FLOAT_CHOICES)
+
+    # free_link = models.CharField(_("link"), max_length=2000, blank=True, null=True,
+    #                              help_text=_("if present image will be clickable"))
+    # page_link = PageField(null=True, blank=True,
+    #                       help_text=_("if present image will be clickable"),
+    #                       verbose_name=_("page link"))
+    # file_link = FilerFileField(
+    #     null=True,
+    #     blank=True,
+    #     default=None,
+    #     verbose_name=_("file link"),
+    #     help_text=_("if present image will be clickable"),
+    #     related_name='+',
+    #     on_delete=models.SET_NULL,
+    # )
+    # original_link = models.BooleanField(_("link original image"), default=False,
+    #                                     help_text=_("if present image will be clickable"))
+    # description = models.TextField(_("description"), blank=True, null=True)
+    # target_blank = models.BooleanField(_('Open link in new window'), default=False)
+    # link_attributes = AttributesField(excluded_keys=EXCLUDED_KEYS, blank=True,
+    #                                   help_text=_('Optional. Adds HTML attributes to the rendered link.'))
+    # cmsplugin_ptr = models.OneToOneField(
+    #     to=CMSPlugin,
+    #     related_name='%(app_label)s_%(class)s',
+    #     parent_link=True,
+    # )
+
+    # we only add the image to select_related. page_link and file_link are FKs
+    # as well, but they are not used often enough to warrant the impact of two
+    # additional LEFT OUTER JOINs.
+    # objects = FilerPluginManager(select_related=('image',))
+
     def __str__(self):
         return self.get_display_name()
 
@@ -82,5 +156,8 @@ class CaroselImageFolder(CMSPlugin):
             return self.title
         elif self.folder_id and self.folder.name:
             return self.folder.name
-        return "<empty>"
-    
+        return _("<empty>")
+
+    class Meta:
+        verbose_name = _('Carousel Image Folder')
+        verbose_name_plural = _('Image Folder Carousels')
