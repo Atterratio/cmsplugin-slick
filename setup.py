4cb6de218619736146d99b8c638cca458f79feeb
paramono <alex@paramono.com>
2017-04-29 02:41:51 +0300

    Alter setup.py

--- setup.py
+++ setup.py
@@ -1,109 +1,53 @@
 # Always prefer setuptools over distutils
-from setuptools import setup, find_packages
+from setuptools import setup
 # To use a consistent encoding
 from codecs import open
 from os import path
 
-here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+# with open('HISTORY.rst') as history_file:
+#     history = history_file.read()
+
+requirements = [
+    'Django>=1.9',
+    'django-cms>=3',
+    'django-filer'
+]
+
 
 setup(
     name='cmsplugin-slick',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1',
-
+    version='0.2.1',
     description='Django-CMS plugin for slick carousel',
-    long_description=open('README.md').read(),
-
-    # The project's main homepage.
-    url='https://github.com/Atterratio/cmsplugin-slick',
-
-    # Author details
-    author='Aeternus Atterratio',
-    author_email='atterratio@gmail.com',
-
-    # Choose your license
-    license='GPLv3+',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    long_description=readme,
+    author='Alexander Paramonov',
+    author_email='alex@paramono.com',
+    # long_description=readme + '\n\n' + history,
+    url='https://github.com/paramono/cmsplugin-slick',
+    packages=[
+        'cmsplugin_slick'
+    ],
+    package_dir={'cmsplugin_slick':
+                 'cmsplugin_slick'},
+    include_package_data=True,
+    install_requires=requirements,
+    keywords='Django CMS slick slider plugin',
     classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
-        #'Intended Audience :: Developers',
-        #'Topic :: Software Development :: Build Tools',
-
-        # Pick your license as you wish (should match "license" above)
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
         'License :: OSI Approved :: GNU Lesser General Public License v3 or later',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        # TODO Testing Python 2
-        #'Programming Language :: Python :: 2',
-        #'Programming Language :: Python :: 2.6',
-        #'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
-
-    # What does your project relate to?
-    keywords='Django Django-CMS JavaScript',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(),
-    include_package_data=True,
-
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['Django>=1.7', 'django-cms>=3', 'django-filer'],
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    #extras_require={
-    #    'dev': ['check-manifest'],
-    #    'test': ['coverage'],
-    #},
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    #package_data={
-    #    'sample': ['package_data.dat'],
-    #},
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    #data_files=[('my_data', ['data/data_file'])],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    #entry_points={
-    #    'console_scripts': [
-    #        'sample=sample:main',
-    #    ],
-    #},
+    license='GPLv3+',
 )
